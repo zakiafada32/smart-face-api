fe304d23c69be543e9a2fac3b279dc1358665a4d
Zaki Afada <afadazaki32@gmail.com>
2020-08-23 19:35:12 +0700

    change npm start

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`app is running on port 3000 ${process.env.PORT}`);
 });
